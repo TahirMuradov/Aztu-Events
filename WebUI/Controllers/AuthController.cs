87e6063f6f2184945c3077786edcbaf0568490c1
TahirMuradov <124434370+TahirMuradov@users.noreply.github.com>
2024-04-23 15:31:21 +0400

    AuthControllerAddedView

---
+++ WebUI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
