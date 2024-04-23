68895c6fda61c750f5c5d122f5092ae0f2ba9748
TahirMuradov <124434370+TahirMuradov@users.noreply.github.com>
2024-04-23 12:46:25 +0400

    MultiLaunguageAdded

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,10 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+using System.Reflection;
+using WebUI.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+#region Localizer
+builder.Services.AddSingleton<LanguageService>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddMvcLocalization().AddDataAnnotationsLocalization(options => options.DataAnnotationLocalizerProvider = (type, factory) =>
+{
+    var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+    return factory.Create(nameof(SharedResource), assemblyName.Name);
+});
+builder.Services.AddControllersWithViews().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportCulture = new List<CultureInfo>
+    {
+        new CultureInfo("az"),
+        new CultureInfo("en"),
+        new CultureInfo("ru"),
+    };
+    options.DefaultRequestCulture = new RequestCulture(supportCulture[0], supportCulture[0]);
+    options.SupportedCultures = supportCulture;
+    options.SupportedUICultures = supportCulture;
+    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+});
+#endregion
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
